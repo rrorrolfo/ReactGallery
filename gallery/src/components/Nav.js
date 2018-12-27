7b5b61ad55b8083d6d5a907c27157b4764939ee1
Rodolfo Perez <rodolfobeste@gmail.com>
2018-12-27 06:24:21 +0200

    Created and styled stateless core components

---
+++ gallery/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li><a href="#">Ducks</a></li>
+                <li><a href="#">Dogs</a></li>
+                <li><a href="#">Birds</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
---
+++ gallery/src/components/Photocontainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const photoContainer = () => {
+    return (
+        <div className="photo-container">
+            <h2>Results</h2>
+        </div>
+    );
+}
+
+export default photoContainer;
