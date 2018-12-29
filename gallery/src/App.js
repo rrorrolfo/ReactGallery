4318051cb557b088918df29f6bd97d16c789f8eb
Rodolfo Perez <rodolfobeste@gmail.com>
2018-12-29 21:51:14 +0200

    Added Flickr API key and optimized styling of the layout

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
+import {
+  BrowserRouter,
+  Route
+} from "react-router-dom";
+import apiKey from "./config";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Nav from "./components/Nav";
 import Photocontainer from "./components/Photocontainer";
 
+
 class App extends Component {
+
   render() {
     return (
-      <div className="container">
-        <Header />
-        <Search />
-        <Nav />
-        <Photocontainer />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Search />
+          <Nav />
+          <Photocontainer />
+        </div>
+      </BrowserRouter>
     )
   }
 }
