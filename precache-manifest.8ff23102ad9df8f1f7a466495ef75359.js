9eca227487d8e7f37f89d92a6605c24290ce180d
Rodolfo Perez <rodolfobeste@gmail.com>
2019-01-18 05:47:10 +0200

    Updates

---
+++ precache-manifest.8ff23102ad9df8f1f7a466495ef75359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a526fc454e3350791f34",
+    "url": "/static/css/main.2718c6f4.chunk.css"
+  },
+  {
+    "revision": "a526fc454e3350791f34",
+    "url": "/static/js/main.a526fc45.chunk.js"
+  },
+  {
+    "revision": "69ef03be183d9dee9202",
+    "url": "/static/js/1.69ef03be.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc61bfe408bb0a4fc35e37a8c42d63ee",
+    "url": "/static/media/bk-img.cc61bfe4.jpg"
+  },
+  {
+    "revision": "86f92811b76facc452dfa177cef83bd1",
+    "url": "/index.html"
+  }
+];
